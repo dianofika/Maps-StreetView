e3b49496211476ebd44764b01e9627d13c2a48f2
dianofika <diannofika.dn@gmail.com>
2017-10-14 07:19:13 +0700

    Final Commit

--- app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl210/streetview/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xiirpl210/streetview/MainActivity.java
@@ -3,11 +3,33 @@ package id.sch.smktelkom_mlg.privateassignment.xiirpl210.streetview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
+        //set the panorama location on startup, when no
+        // panoramas have been loaded
+
+        panorama.setPosition(new LatLng(37.400546, -122.108668));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera, 10000);
     }
 }
